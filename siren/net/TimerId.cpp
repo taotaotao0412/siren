d874b7ac0c1bb1caf24fa05b0de40bf3829fb5c7
kami <taotaotao0412@gmail.com>
2023-09-12 13:47:21 +0800

    remove old project structure

--- siren/net/TimerId.cpp
+++
@@ -1,5 +0,0 @@
-#include "siren/net/TimerId.h"
-
-siren::net::TimerId::TimerId() : timer_(nullptr), sequence_(0) {}
-
-siren::net::TimerId::TimerId(Timer* timer, int64_t seq):timer_(timer), sequence_(seq) {}
